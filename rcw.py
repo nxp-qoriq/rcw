b3b2e856ae64457da8251638423ead7747155c03
Timur Tabi <timur@freescale.com>
2012-06-18 16:31:27 -0500

    rcw.py: add BSD license
    Signed-off-by: Timur Tabi <timur@freescale.com>

--- rcw.py
+++ rcw.py
@@ -5,6 +5,28 @@
 # Copyright 2011 Freescale Semiconductor, Inc.
 # Author: Timur Tabi <timur@freescale.com>
 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#     * Neither the name of Freescale Semiconductor nor the
+#     names of its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written permission.
+
+# This software is provided by Freescale Semiconductor "as is" and any
+# express or implied warranties, including, but not limited to, the implied
+# warranties of merchantability and fitness for a particular purpose are
+# disclaimed. In no event shall Freescale Semiconductor be liable for any
+# direct, indirect, incidental, special, exemplary, or consequential damages
+# (including, but not limited to, procurement of substitute goods or services;
+# loss of use, data, or profits; or business interruption) however caused and
+# on any theory of liability, whether in contract, strict liability, or tort
+# (including negligence or otherwise) arising in any way out of the use of
+# this software, even if advised of the possibility of such damage.
+
 # This program compiles an RCW "source file" into a binary image, with or
 # without a PBL header.  The syntax of the source file is very simple.  These
 # are the only constructs supported:
